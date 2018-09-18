9106b3bd0cdbb574e801ffa11f8265d979e46018
Jon Hayes <jhayes@mvnp.com>
2018-09-17 14:18:45 -1000

    change the title text

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,11 +18,11 @@ const config = {
   ** Headers of the page
   */
   head: {
-    title: 'Custom app with Contentful',
+    title: 'Jon\'s World',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Custom app in under 5 minutes' }
+      { hid: 'description', name: 'description', content: 'The personal ramblings of Jonathan Hayes - web developer.' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
